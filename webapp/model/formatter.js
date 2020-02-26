4ca68adbc17e3d18b7aabe1ee584f2a1ec1b2e97
Николай <nickcode@mail.ru>
2020-02-26 08:53:55 +0000

    second

--- webapp/model/formatter.js
+++ webapp/model/formatter.js
@@ -1,9 +1,12 @@
 /* global moment:true */
+/*eslint consistent-this: ["error", "oElem"]*/
 
 sap.ui.define([
-	"momentjs/nickcode/model/moment-with-locales",
+	//	"momentjs/nickcode/model/moment-with-locales",
 	"sap/ui/Device"
-], function (momentjs, Device) {
+], function (
+	//momentjs,
+	Device) {
 	"use strict";
 
 	var Formatter = {
@@ -11,10 +14,14 @@ sap.ui.define([
 			if (!sValue) {
 				return "";
 			}
-			moment.locale('ru');
+
+			//Локализация - Россия
+			moment.locale("ru");
+
 			var nowDate = moment();
 			var date = moment(sValue);
-			var yesterday = moment(sValue).subtract(1, 'days');
+			var yesterday = moment(sValue).subtract(1, "days");
+			
 			var oElem = this;
 
 			if (nowDate.format("DD.MM.YY") === date.format("DD.MM.YY")) {
@@ -22,25 +29,28 @@ sap.ui.define([
 
 				if ((diffMs) <= (1000 * 60 * 60) && !Device.system.phone) {
 
+					//Будем обновлять состояние каждые 60 секунд
 					setInterval(function () {
 						try {
 							var time = moment(oElem.getModel("nickcode").getData().time);
-							var diffMs = nowDate.diff(time);
+							diffMs = nowDate.diff(time);
 							if (diffMs < 3600000) {
 								var diffTime = time.fromNow();
 								oElem.setText(diffTime);
 							}
-						} catch (err) {}
+						} catch (err) {
+							//console.log("error");
+						}
 					}, 60000);
 
 					return date.fromNow();
 				} else {
-					return date.format('HH:mm');
+					return date.format("HH:mm");
 				}
 			} else if (nowDate.format("DD.MM.YY") === yesterday.format("DD.MM.YY")) {
-				return 'Вчера';
+				return "Вчера";
 			} else {
-				return date.format('DD MMM');
+				return date.format("DD MMM");
 			}
 
 		}
